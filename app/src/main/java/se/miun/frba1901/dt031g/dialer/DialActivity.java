98d089b3e6aec384cb5c167137d4102655e27b83
Fredrik Bäck <frba1901@student.miun.se>
2021-04-07 16:38:40 +0200

    Dial activity klar

---
+++ app/src/main/java/se/miun/frba1901/dt031g/dialer/DialActivity.java
@@ -0,0 +1,14 @@
+package se.miun.frba1901.dt031g.dialer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dial);
+    }
+}
--- app/src/main/java/se/miun/frba1901/dt031g/dialer/MainActivity.java
+++ app/src/main/java/se/miun/frba1901/dt031g/dialer/MainActivity.java
@@ -2,7 +2,10 @@ package se.miun.frba1901.dt031g.dialer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void startDialer(View view){
+        Intent intent = new Intent(this, DialActivity.class);
+        startActivity(intent);
     }
 }
