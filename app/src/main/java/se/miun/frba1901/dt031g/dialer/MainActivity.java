910c683b12d53f347a4d5586be4af92485856dfc
Fredrik Bäck <frba1901@student.miun.se>
2021-06-04 18:05:43 +0200

    Task 1 färdigt

--- app/src/main/java/se/miun/frba1901/dt031g/dialer/MainActivity.java
+++ app/src/main/java/se/miun/frba1901/dt031g/dialer/MainActivity.java
@@ -6,13 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean aboutOpened = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putBoolean("ABOUT_OPENED", aboutOpened); // Sparar om vi har öppnat about eller inte
+        super.onSaveInstanceState(savedInstanceState);
+    }
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        aboutOpened = savedInstanceState.getBoolean("ABOUT_OPENED");
     }
     public void startDialer(View view){
         Intent intent = new Intent(this, DialActivity.class);
@@ -31,7 +42,12 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
     public void startAbout(View view){
-        AboutDialog aboutDialog = new AboutDialog();
-        aboutDialog.show(getSupportFragmentManager(), "About dialog");
+        if(aboutOpened)
+            Toast.makeText(getApplicationContext(), "You have already seen the about section",Toast.LENGTH_LONG).show();
+        else {
+            AboutDialog aboutDialog = new AboutDialog();
+            aboutDialog.show(getSupportFragmentManager(), "About dialog");
+            aboutOpened = true; // Anger att vi har öppnat about
+        }
     }
 }
