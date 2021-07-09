0f36671e5ad27fda9885f771cea77d7711d9d780
Fredrik Bäck <frba1901@student.miun.se>
2021-07-09 17:11:08 +0200

    Assignment 7 klar

--- app/src/main/java/se/miun/frba1901/dt031g/dialer/MapsActivity.java
+++ app/src/main/java/se/miun/frba1901/dt031g/dialer/MapsActivity.java
@@ -3,15 +3,26 @@ package se.miun.frba1901.dt031g.dialer;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -45,9 +56,23 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng latLng1 = new LatLng(55.001099, 11.10694);
+        LatLng latlng2 = new LatLng(69.063141, 24.16707);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(new LatLngBounds(latLng1, latlng2),0));
+
+        // Hämtar alla nummer och sätter ut markeringar för dessa
+        DialDatabaseManager.getInstance(this).getAll().observe(this, dialnumbers -> {
+            for(DialNumber dialNumber : dialnumbers){
+                // Om nummret inte har lat/long så markerar vi inte det
+                if(dialNumber.latitude != null && dialNumber.longitude != null) {
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.title(dialNumber.number);
+                    markerOptions.snippet(dialNumber.date);
+                    markerOptions.position(new LatLng(dialNumber.latitude, dialNumber.longitude));
+                    markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                    mMap.addMarker(markerOptions);
+                }
+            }
+        });
     }
 }
